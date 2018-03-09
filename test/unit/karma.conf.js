1fdf0d08dfbff027516d86b5ed405d91fee2d7ff
hzshaoyy <hzshaoyy@corp.netease.com>
2018-03-09 11:28:21 +0800

    @dev rm coverage

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function karmaConfig (config) {
     // 2. add it to the `browsers` array below.
     browsers: ['PhantomJS'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
-    reporters: ['spec', 'coverage', 'coveralls'],
+    reporters: ['spec', 'coverage'],
     files: ['./index.js'],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
